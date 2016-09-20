96867e2f5a00c4b2bbe0f902c67e780a316e367f
fabgarsanOdeco <fabio.garcia.sanchez@gmail.com>
2016-09-20 13:18:50 -0500

    Lista precios 2

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,16 +4,10 @@ from django.contrib.auth.models import User
 from empresas.models import Empresa
 # Create your models here.
 
-def avatar_upload_to(instance, filename):
-    username = instance.user.username
-    basename, file_extention = filename.split(".")
-    new_filename = "%s_%s.%s" % (username, basename, file_extention)
-    return "user/avatar/%s/%s" % (username, new_filename)
-
 class UserExtended(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     tipo = models.CharField(max_length=1, choices=((('I', 'Colaborador'),('E', 'Cliente'))))
-    avatar = models.ImageField(blank=True, upload_to=avatar_upload_to)
+
 
     def __str__(self):
         return self.user.first_name
