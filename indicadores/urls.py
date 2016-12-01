13fd9e35d05ea2a38bd1216e97a439ab9d3a85b6
fabgarsanOdeco <fabio.garcia.sanchez@gmail.com>
2016-12-01 15:59:20 -0500

    Reporte de ventas por cliente

--- indicadores/urls.py
+++ indicadores/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import url
 
-from .views import VentasVendedor, FacturacionAno, FacturacionAnoLinea
+from .views import (
+    VentasVendedor,
+    FacturacionAno,
+    FacturacionAnoLinea,
+    VentasClientes,
+    VentasClientesAno
+)
 
 urlpatterns = [
     url(r'^ventxvend/', VentasVendedor.as_view(), name='ventasxvendedor'),
+    url(r'^ventxclie/', VentasClientes.as_view(), name='ventasxcliente'),
+    url(r'^ventxcliexano/', VentasClientesAno.as_view(), name='ventasxclientexano'),
     url(r'^factxano/', FacturacionAno.as_view(), name='facturacionxano'),
     url(r'^factxanoxlinea/', FacturacionAnoLinea.as_view(), name='facturacionxanoxlinea'),
 ]
--- indicadores/views.py
+++ indicadores/views.py
@@ -56,6 +56,116 @@ class VentasVendedor(JSONResponseMixin, AjaxResponseMixin, TemplateView):
         print(qs.all().count())
         return qs
 
+class VentasClientes(JSONResponseMixin, AjaxResponseMixin, TemplateView):
+    template_name = 'indicadores/ventasxcliente.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        ano_fin = MovimientoVentaBiable.objects.all().aggregate(Max('year'))['year__max']
+        ano_ini = MovimientoVentaBiable.objects.all().aggregate(Min('year'))['year__min']
+        ano_fin = ano_fin + 1
+
+        context['anos_list'] = list(range(ano_ini, ano_fin))
+
+        return context
+
+    def post_ajax(self, request, *args, **kwargs):
+        ano = self.request.POST.getlist('anos[]')
+        mes = self.request.POST.getlist('meses[]')
+        linea = self.request.POST.get('linea')
+
+        qs = self.consulta(ano, mes)
+
+        if not linea == "0":
+            qs = qs.filter(vendedor__linea=linea)
+
+        lista = list(qs)
+        for i in lista:
+            i["v_bruta"] = int(i["v_bruta"])
+            i["Costo"] = int(i["Costo"])
+            i["v_neto"] = int(i["v_neto"])
+            i["renta"] = int(i["renta"])
+            i["Descuentos"] = int(i["Descuentos"])
+        return self.render_json_response(lista)
+
+    def consulta(self, ano, mes):
+        qs = MovimientoVentaBiable.objects.all().values('cliente').annotate(
+            v_bruta=Sum('venta_bruta'),
+            v_neto=Sum('venta_neto'),
+            Descuentos=Sum('dscto_netos'),
+            Costo=Sum('costo_total'),
+            renta=Sum('rentabilidad'),
+            Margen=(Sum('rentabilidad') / Sum('venta_bruta') * 100),
+            linea=Case(
+                When(vendedor__linea=1, then=Value('Proyectos')),
+                When(vendedor__linea=2, then=Value('Bandas y Componentes')),
+                default=Value('Posventa'),
+                output_field=CharField(),
+            ),
+        ).filter(
+            year__in=list(map(lambda x: int(x), ano)),
+            month__in=list(map(lambda x: int(x), mes))
+        ).order_by('-v_bruta')
+        print(qs.all().count())
+        return qs
+
+
+class VentasClientesAno(JSONResponseMixin, AjaxResponseMixin, TemplateView):
+    template_name = 'indicadores/ventasxclientexano.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        ano_fin = MovimientoVentaBiable.objects.all().aggregate(Max('year'))['year__max']
+        ano_ini = MovimientoVentaBiable.objects.all().aggregate(Min('year'))['year__min']
+        ano_fin = ano_fin + 1
+
+        context['anos_list'] = list(range(ano_ini, ano_fin))
+
+        return context
+
+    def post_ajax(self, request, *args, **kwargs):
+        ano = self.request.POST.getlist('anos[]')
+        mes = self.request.POST.getlist('meses[]')
+        linea = self.request.POST.get('linea')
+
+        qs = self.consulta(ano, mes)
+
+        if not linea == "0":
+            qs = qs.filter(vendedor__linea=linea)
+
+        lista = list(qs)
+        for i in lista:
+            i["v_bruta"] = int(i["v_bruta"])
+            i["Costo"] = int(i["Costo"])
+            i["v_neto"] = int(i["v_neto"])
+            i["renta"] = int(i["renta"])
+            i["Descuentos"] = int(i["Descuentos"])
+        return self.render_json_response(lista)
+
+    def consulta(self, ano, mes):
+        qs = MovimientoVentaBiable.objects.all().values('cliente', 'year').annotate(
+            v_bruta=Sum('venta_bruta'),
+            v_neto=Sum('venta_neto'),
+            Descuentos=Sum('dscto_netos'),
+            Costo=Sum('costo_total'),
+            renta=Sum('rentabilidad'),
+            Margen=(Sum('rentabilidad') / Sum('venta_bruta') * 100),
+            linea=Case(
+                When(vendedor__linea=1, then=Value('Proyectos')),
+                When(vendedor__linea=2, then=Value('Bandas y Componentes')),
+                default=Value('Posventa'),
+                output_field=CharField(),
+            ),
+        ).filter(
+            year__in=list(map(lambda x: int(x), ano)),
+            month__in=list(map(lambda x: int(x), mes))
+        ).order_by('-v_bruta')
+        print(qs.all().count())
+        print(qs)
+        return qs
+
 
 class FacturacionAno(JSONResponseMixin, AjaxResponseMixin, TemplateView):
     template_name = 'indicadores/facturacionxano.html'
@@ -76,6 +186,7 @@ class FacturacionAno(JSONResponseMixin, AjaxResponseMixin, TemplateView):
         qs = self.consulta(ano)
 
         if not linea == "0":
+            print('entro')
             qs = qs.filter(vendedor__linea=linea)
 
         print(qs.all().count())
@@ -87,6 +198,7 @@ class FacturacionAno(JSONResponseMixin, AjaxResponseMixin, TemplateView):
             i["v_neto"] = int(i["v_neto"])
             i["renta"] = int(i["renta"])
             i["Descuentos"] = int(i["Descuentos"])
+            i["Impuestos"] = int(i["Impuestos"])
         return self.render_json_response(lista)
 
     def consulta(self, ano):
@@ -96,6 +208,7 @@ class FacturacionAno(JSONResponseMixin, AjaxResponseMixin, TemplateView):
             Descuentos=Sum('dscto_netos'),
             Costo=Sum('costo_total'),
             renta=Sum('rentabilidad'),
+            Impuestos=Sum('imp_netos'),
             Margen=(Sum('rentabilidad') / Sum('venta_bruta') * 100)
         ).filter(year=ano).order_by('month')
         return qs
@@ -128,92 +241,4 @@ class FacturacionAnoLinea(TemplateView):
             Margen=(Sum('rentabilidad') / Sum('venta_neto') * 100)
         ).filter(year=ano)
 
-        # if qs.exists():
-        #     print(qs.all().count())
-        #     df = pd.DataFrame.from_records(qs)
-        #     #df[['v_neto', 'v_bruta']].apply(pd.to_numeric)
-        #     df[['v_neto', 'v_bruta']].apply(lambda x: pd.to_numeric(x, errors='ignore'))
-        #
-        #     print(df)
-        #
-        #     df.rename(
-        #         columns={
-        #             'month': 'Mes',
-        #             'renta': 'Rent.',
-        #             'v_neto': 'Vr. Neto.',
-        #             'v_bruta': 'Vr. Bruto.',
-        #         },
-        #         inplace=True)
-        #
-        #     table = pivot_table(df,
-        #                         values=['Vr. Bruto.', 'Descuentos', 'Vr. Neto.', 'Costo', 'Rent.','Margen'],
-        #                         columns=['Mes'],
-        #                         aggfunc=np.sum,
-        #                         fill_value=0,
-        #                         dropna=True
-        #                         )
-        #
-        #     tableF = table.reindex_axis(['Vr. Bruto.', 'Descuentos', 'Vr. Neto.', 'Costo', 'Rent.', 'Margen'], axis=0)
-        #
-        #     context['tabla_consulta'] = tableF.to_html(classes="table table-striped")
-        #     context['ano_filtro'] = ano
-
         return context
-
-
-
-
-
-        # # Create your views here.
-        # class VentasVendedor(TemplateView):
-        #     template_name = 'indicadores/ventasxvendedor.html'
-        #
-        #     def get_context_data(self, **kwargs):
-        #         context = super().get_context_data(**kwargs)
-        #         qs = MovimientoVentaBiable.objects.values('vendedor').annotate(
-        #             mes=TruncMonth('fecha'),
-        #             ano=TruncYear('fecha'),
-        #             vendedor_nombre=F('vendedor__nombre'),
-        #             renta=Sum('rentabilidad'),
-        #             v_neto=Sum('venta_neto')
-        #         )
-        #         # pd.options.display.float_format = '${:,.2f}'.format
-        #         # df['cost'] = df['cost'].map('${:,.2f}'.format)ss
-        #
-        #         # output = df.to_html(formatters={
-        #         #     'var1': '{:,.2f}'.format,
-        #         #     'var2': '{:,.2f}'.format,
-        #         #     'var3': '{:,.2%}'.format
-        #         # })
-        #
-        #         print(qs.all().count())
-        #
-        #
-        #         df = pd.DataFrame.from_records(qs)
-        #
-        #         df['mes'] = df['mes'].dt.month
-        #         df['ano'] = df['ano'].dt.year
-        #
-        #         df.rename(
-        #             columns={
-        #                 'vendedor_nombre': 'Vendedor',
-        #                 'ano': 'Año',
-        #                 'mes': 'Mes',
-        #                 'renta': 'Rent.',
-        #                 'v_neto': 'Vr. Neto.',
-        #             },
-        #             inplace=True)
-        #
-        #
-        #         table = pivot_table(df,
-        #                             index=['Vendedor', 'Mes'],
-        #                             values=['Rent.', 'Vr. Neto.'],
-        #                             columns=['Año'],
-        #                             aggfunc={'Vr. Neto.': np.sum, 'Rent.': np.sum},
-        #                             fill_value=0,
-        #                             dropna=True
-        #                             )
-        #         #table['margen'] = (table['Rent.'] / table['Vr. Neto.']) * 100
-        #
-        #         context['tabla_consulta'] = table.to_html(classes="table table-striped")
-        #         return context
