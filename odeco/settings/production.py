d5ed234fe873d50f4f55a7d2e2f3112146ef56fa
fabgarsanOdeco <fabio.garcia.sanchez@gmail.com>
2016-12-16 15:49:40 -0500

    Se cambia la cotizacion de aplicacion y ya funciona con articulos de catalogo

--- odeco/settings/production.py
+++ odeco/settings/production.py
@@ -6,6 +6,7 @@ import os
 from .base import *
 
 if not DEBUG:
+    DEBUG=True
     ########## MANAGER CONFIGURATION
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#admins
     ADMINS = (
