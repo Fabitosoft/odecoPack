585c6ad188cb4d71352c2ced578d2540cabeda70
fabgarsanOdeco <fabio.garcia.sanchez@gmail.com>
2016-12-28 09:07:38 -0500

    Se agrega activo e inactivo a los vendedores 3

--- biable/admin.py
+++ biable/admin.py
@@ -6,7 +6,7 @@ from biable.models import VendedorBiable, VendedorBiableUser
 class VendedorBiableAdmin(admin.ModelAdmin):
     list_display = ('nombre','id','linea','activo')
     list_editable = ('linea')
-    readonly_fields = ('activo')
+    readonly_fields = ('activo',)
 
 admin.site.register(VendedorBiable,VendedorBiableAdmin)
 admin.site.register(VendedorBiableUser)
