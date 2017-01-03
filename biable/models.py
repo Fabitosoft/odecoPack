5a7b4a9f9c1810f1888d4f3319ab81e65de1cd11
fabgarsanOdeco <fabio.garcia.sanchez@gmail.com>
2017-01-03 15:56:42 -0500

    se coloca nit de cliente como llave

--- biable/models.py
+++ biable/models.py
@@ -8,7 +8,7 @@ from usuarios.models import UserExtended
 
 # Create your models here.
 class Cliente(TimeStampedModel):
-    nit = models.CharField(max_length=20)
+    nit = models.CharField(max_length=20, primary_key=True)
     nombre = models.CharField(max_length=120)
 
     def __str__(self):
