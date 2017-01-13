3c11509501071aa707b33a2383adb94d893c5624
fabgarsanOdeco <fabio.garcia.sanchez@gmail.com>
2017-01-13 15:49:59 -0500

    Se agrega geografía colombia para tener la base de datos de ciudades y departamentos para los envíos y cotizaciones 2

--- geografia_colombia/models.py
+++ geografia_colombia/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 # Create your models here.
 class Departamento(models.Model):
-    id = models.PositiveIntegerField(primary_key=True)
     nombre = models.CharField(max_length=120)
 
     def __str__(self):
