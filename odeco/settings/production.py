8b34672be458c3d2d91e7486335550d18fdc1116
fabgarsanOdeco <fabio.garcia.sanchez@gmail.com>
2017-01-17 18:10:50 -0500

    Tratando de arreglar el envio de correo 2

--- odeco/settings/production.py
+++ odeco/settings/production.py
@@ -111,7 +111,7 @@ if not DEBUG:
     EMAIL_PORT_ODECO = os.environ['EMAIL_PORT_ODECO_VENTAS']
 
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-use-tls
-    EMAIL_USE_TLS_ODECO = False
+    EMAIL_USE_TLS_ODECO = str_to_bool(os.environ['EMAIL_TLS_ODECO_VENTAS'])
 
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#server-email
     SERVER_EMAIL_ODECO = EMAIL_HOST_USER_ODECO
