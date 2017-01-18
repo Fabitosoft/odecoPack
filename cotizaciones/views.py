a1e1c48bb777474b1fe4b4170163aff057c15c9b
fabgarsanOdeco <fabio.garcia.sanchez@gmail.com>
2017-01-18 15:45:57 -0500

    Tratando de arreglar el envio de correo 5

--- cotizaciones/views.py
+++ cotizaciones/views.py
@@ -327,7 +327,7 @@ class EmailPrueba(View):
 
         output = BytesIO()
         HTML(string=html_content).write_pdf(target=output)
-        msg = EmailMultiAlternatives(subject, text_content, from_email, to=[to], bcc=[user.email],
+        msg = EmailMultiAlternatives(subject, text_content,'ODECOPACK / VENTAS', from_email, to=[to], bcc=[user.email],
                                      connection=connection)
         msg.attach(nombre_archivo_cotizacion, output.getvalue(), 'application/pdf')
         msg.send()
