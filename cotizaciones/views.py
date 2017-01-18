22e74770d9f1ac03faa8b4f6bcf727a305c83d02
fabgarsanOdeco <fabio.garcia.sanchez@gmail.com>
2017-01-18 12:22:20 -0500

    Tratando de arreglar el envio de correo 5

--- cotizaciones/views.py
+++ cotizaciones/views.py
@@ -278,19 +278,15 @@ class EmailPrueba(View):
         ctx = {
             'object': obj,
         }
-        text_content = render_to_string('cotizaciones/emails/cotizacion.html', ctx)
+        text_content = "Adjunto la cotizacion solicitada en PDF"
         html_content = get_template('cotizaciones/emails/cotizacion.html').render(Context(ctx))
 
         output = BytesIO()
         HTML(string=html_content).write_pdf(target=output)
         msg = EmailMultiAlternatives(subject, text_content, from_email=from_email, to=[to],
                                      connection=connection)
-        msg.attach_alternative(html_content, "text/html")
-        msg.content_subtype = 'html'  # Main content is text/html
-        msg.mixed_subtype = 'related'  # This is critical, otherwise images will be displayed as attachments!
 
         nombre_archivo_cotizacion = "Cotizacion Odecopack - CB %s.pdf" % (obj.id)
-
         msg.attach(nombre_archivo_cotizacion, output.getvalue(), 'application/pdf')
         msg.send()
         output.close()
