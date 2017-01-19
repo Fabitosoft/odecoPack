b3107cce499697a133a47cba911dbe07b76c4c50
fabgarsanOdeco <fabio.garcia.sanchez@gmail.com>
2017-01-19 10:17:33 -0500

    se agregan items desde el biable

--- biable/admin.py
+++ biable/admin.py
@@ -1,34 +1,47 @@
 from django.contrib import admin
 
-from biable.models import VendedorBiable, LineaVendedorBiable,Cliente, FacturasBiable
+from biable.models import VendedorBiable, LineaVendedorBiable, Cliente, FacturasBiable, ItemsBiable
+
+
 # Register your models here.
+
+class ItemsBiableAdmin(admin.ModelAdmin):
+    list_display = (
+        'id_item', 'id_referencia', 'descripcion', 'descripcion_dos', 'activo', 'nombre_tercero', 'desc_item_padre',
+        'unidad_medida_inventario', 'id_procedencia')
+    readonly_fields = (
+    'id_item', 'id_referencia', 'descripcion', 'descripcion_dos', 'activo', 'nombre_tercero', 'desc_item_padre',
+    'unidad_medida_inventario', 'id_procedencia')
+
 
 class VendedorBiableAdmin(admin.ModelAdmin):
     list_select_related = ['linea_ventas', 'colaborador']
-    list_display = ('nombre','id','linea_ventas','activo','colaborador')
-    list_editable = ('linea_ventas','colaborador')
+    list_display = ('nombre', 'id', 'linea_ventas', 'activo', 'colaborador')
+    list_editable = ('linea_ventas', 'colaborador')
     readonly_fields = ('activo',)
 
-    def get_linea_ventas(self,obj):
+    def get_linea_ventas(self, obj):
         return obj.linea_ventas.nombre
+
     get_linea_ventas.short_description = 'Línea'
 
 
 class ClienteBiableAdmin(admin.ModelAdmin):
-    list_display = ('nit','nombre')
+    list_display = ('nit', 'nombre')
 
     search_fields = [
         'nit',
         'nombre',
     ]
 
-    readonly_fields = ('nit','nombre',)
+    readonly_fields = ('nit', 'nombre',)
+
 
 class FacturasBiableAdmin(admin.ModelAdmin):
-    list_select_related = ['cliente','vendedor']
-    list_filter = ('tipo_documento','year','month','day','vendedor')
-    search_fields = ('nro_documento','tipo_documento','cliente__nombre')
-    list_display = ('nro_documento','tipo_documento','cliente','vendedor')
+    list_select_related = ['cliente', 'vendedor']
+    list_filter = ('tipo_documento', 'year', 'month', 'day', 'vendedor')
+    search_fields = ('nro_documento', 'tipo_documento', 'cliente__nombre')
+    list_display = ('nro_documento', 'tipo_documento', 'cliente', 'vendedor')
     readonly_fields = (
         'year',
         'month',
@@ -45,7 +58,8 @@ class FacturasBiableAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(Cliente,ClienteBiableAdmin)
-admin.site.register(VendedorBiable,VendedorBiableAdmin)
+admin.site.register(Cliente, ClienteBiableAdmin)
+admin.site.register(VendedorBiable, VendedorBiableAdmin)
 admin.site.register(LineaVendedorBiable)
-admin.site.register(FacturasBiable,FacturasBiableAdmin)
+admin.site.register(FacturasBiable, FacturasBiableAdmin)
+admin.site.register(ItemsBiable, ItemsBiableAdmin)
