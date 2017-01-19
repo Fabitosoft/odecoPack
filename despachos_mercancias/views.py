250663d4dc8200b7becb990e4382719edc55b69e
fabgarsanOdeco <fabio.garcia.sanchez@gmail.com>
2017-01-19 08:51:56 -0500

    Se hace obligatoria la sesion para ingresar a ver indicadores3

--- despachos_mercancias/views.py
+++ despachos_mercancias/views.py
@@ -3,32 +3,35 @@ from django.views.generic.edit import UpdateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
-from braces.views import SelectRelatedMixin, PrefetchRelatedMixin
+from braces.views import SelectRelatedMixin, PrefetchRelatedMixin, LoginRequiredMixin
 
 from .models import EnvioTransportadoraTCC
 from .forms import EnvioTccForm
+
+
 # Create your views here.
 
-class EnvioTransportadoraTCCUpdateView(UpdateView):
+class EnvioTransportadoraTCCUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'despachos_mercancias/envio_tcc_update.html'
     model = EnvioTransportadoraTCC
     form_class = EnvioTccForm
 
-class EnvioTransportadoraTCCDetailView(PrefetchRelatedMixin,SelectRelatedMixin,DetailView):
+
+class EnvioTransportadoraTCCDetailView(LoginRequiredMixin, PrefetchRelatedMixin, SelectRelatedMixin, DetailView):
     template_name = 'despachos_mercancias/envio_tcc_detail.html'
     model = EnvioTransportadoraTCC
-    select_related = ["ciudad","ciudad__departamento","cliente"]
-    prefetch_related = ["facturas","facturas__cliente","facturas__vendedor"]
+    select_related = ["ciudad", "ciudad__departamento", "cliente"]
+    prefetch_related = ["facturas", "facturas__cliente", "facturas__vendedor"]
 
-class EnvioTransportadoraTCCReporteView(SelectRelatedMixin,TemplateView):
+
+class EnvioTransportadoraTCCReporteView(LoginRequiredMixin, SelectRelatedMixin, TemplateView):
     template_name = 'despachos_mercancias/reporte_seguimiento.html'
-    select_related = ["ciudad", "ciudad__departamento","cliente"]
+    select_related = ["ciudad", "ciudad__departamento", "cliente"]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         qs = EnvioTransportadoraTCC.pendientes
-        context["boom"] = qs.boom().select_related("ciudad", "ciudad__departamento","cliente")
-        context["entrega"] = qs.entrega().select_related("ciudad", "ciudad__departamento","cliente")
-        context["entrega_boom"] = qs.entrega_boom().select_related("ciudad", "ciudad__departamento","cliente")
+        context["boom"] = qs.boom().select_related("ciudad", "ciudad__departamento", "cliente")
+        context["entrega"] = qs.entrega().select_related("ciudad", "ciudad__departamento", "cliente")
+        context["entrega_boom"] = qs.entrega_boom().select_related("ciudad", "ciudad__departamento", "cliente")
         return context
-
