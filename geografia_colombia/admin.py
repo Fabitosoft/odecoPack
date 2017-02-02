b1c8be38b08812a352b148b75c5d57bc380c3350
fabgarsanOdeco <fabio.garcia.sanchez@gmail.com>
2017-02-02 17:01:32 -0500

    Se agregan Países

--- geografia_colombia/admin.py
+++ geografia_colombia/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from import_export.admin import ImportExportModelAdmin
 
-from .models import Departamento, Ciudad
+from .models import Departamento, Ciudad, Pais
 # Register your models here.
 
 class DepartamentoAdmin(ImportExportModelAdmin):
@@ -11,9 +11,10 @@ class DepartamentoAdmin(ImportExportModelAdmin):
 class CiudadAdmin(ImportExportModelAdmin):
     list_select_related = ('departamento',)
     list_display = ('nombre','departamento')
-    list_filter = ('departamento',)
+    list_filter = ('departamento','departamento__pais')
     search_fields = ('nombre','departamento__nombre')
 
 
 admin.site.register(Departamento,DepartamentoAdmin)
+admin.site.register(Pais)
 admin.site.register(Ciudad,CiudadAdmin)
--- geografia_colombia/models.py
+++ geografia_colombia/models.py
@@ -2,8 +2,21 @@ from django.db import models
 
 
 # Create your models here.
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=120)
+
+    class Meta:
+        verbose_name='País'
+        verbose_name_plural='Paises'
+
+    def __str__(self):
+        return self.nombre
+
+
 class Departamento(models.Model):
     nombre = models.CharField(max_length=120)
+    pais = models.ForeignKey(Pais, related_name='mis_departamentos', null=True)
 
     def __str__(self):
         return self.nombre
