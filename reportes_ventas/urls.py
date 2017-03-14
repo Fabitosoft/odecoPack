f352b96598e65586fdd55a313a240e5ce9437da0
fabgarsanOdeco <fabio.garcia.sanchez@gmail.com>
2017-03-14 10:15:37 -0500

    Reporte de Margenes x Factura

--- reportes_ventas/urls.py
+++ reportes_ventas/urls.py
@@ -1,9 +1,34 @@
 from django.conf.urls import url
 
 from .views import (
-    VistaPrueba
+    VentasVendedor,
+    VentasMes,
+    VentasLineaAno,
+    VentasClientes,
+    VentasClientesAno,
+    VentasClienteMes,
+    VentasLineaAnoMes,
+    VentasVendedorMes,
+    VentasVendedorConsola,
+    CarteraVencimientos,
+    TrabajoCotizacionVentaVendedorAnoMes,
+    VentasProductoAnoMes,
+    MargenesFacturaView
 )
 
 urlpatterns = [
-    url(r'^uno/', VistaPrueba.as_view(), name='prueba'),
+    url(r'^ventxvend/', VentasVendedor.as_view(), name='ventasxvendedor'),
+    url(r'^ventxclie/', VentasClientes.as_view(), name='ventasxcliente'),
+    url(r'^ventxcliexano/', VentasClientesAno.as_view(), name='ventasxclientexano'),
+    url(r'^ventxcliexmes/', VentasClienteMes.as_view(), name='ventasxclientexmes'),
+    url(r'^ventxmes/', VentasMes.as_view(), name='ventasxmes'),
+    url(r'^ventxlineaxano/', VentasLineaAno.as_view(), name='ventasxlineaxano'),
+    url(r'^ventxlineaxanoxmes/', VentasLineaAnoMes.as_view(), name='ventasxlineaxanoxmes'),
+    url(r'^ventxvendxmes/', VentasVendedorMes.as_view(), name='ventasxvendedorxmes'),
+    url(r'^ventxproduxmesxano/', VentasProductoAnoMes.as_view(), name='ventasxproductoxanoxmes'),
+    url(r'^consola_ventas/', VentasVendedorConsola.as_view(), name='consolaventas'),
+    url(r'^cartera_vencimientos/', CarteraVencimientos.as_view(), name='cartera_vencimientos'),
+    url(r'^margen_factura/', MargenesFacturaView.as_view(), name='margen_factura'),
+    url(r'^cotizacion_venta_vendedorxanoxmes/', TrabajoCotizacionVentaVendedorAnoMes.as_view(),
+        name='cotizacion_venta_vendedorxanoxmes'),
 ]
